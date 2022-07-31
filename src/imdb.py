7cd40dba4bf6c4f682a2902e1ef70e80c8086976
Rodolfo <rodolfobrandao1995@gmail.com>
2022-07-31 02:05:50 -0300

    Refactor after Pylint scan

--- src/imdb.py
+++ src/imdb.py
@@ -1,79 +1,89 @@
-import requests as r
-from typing import List
+import requests
 
 
 class Imdb:
+    '''
+    A wrapper class for the IMDb API.
+    '''
+
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
         self.base_url = 'https://imdb-api.com/en/API'
 
-    def search_movies(self, name: str) -> List['Movie']:
-        movies = []
+    def search_movies(self, name: str) -> list[dict]:
+        '''
+        Searches for movies whose title matches the specified name.
+
+        Parameters
+        ----------
+        name : str
+            The name to search.
+
+        Returns
+        -------
+        list[dict]
+            A list containing dictionaries with the following properties:
+            id, title, description, image_url.
+        '''
+
+        movies: list[dict] = []
         url = '{0}/SearchMovie/{1}/{2}'.format(
             self.base_url, self.api_key, name)
 
-        response = r.get(url)
+        response = requests.get(url)
 
-        if (response.status_code == 200):
+        if response.status_code == 200:
             response_obj = response.json()
 
             for movie in response_obj:
-                movies.append(Movie(
-                    movie['id'],
-                    movie['title'],
-                    movie['description'],
-                    movie['image']
-                ))
+                movies.append({
+                    'id': movie['id'],
+                    'title': movie['title'],
+                    'description': movie['description'],
+                    'image_url': movie['image']
+                })
 
         return movies
 
-    def search_title(self, title_id: str) -> 'Title':
+    def search_title(self, title_id: str) -> dict:
+        '''
+        Searchs for the title specified by the id.
+
+        Parameters
+        ----------
+        id : str
+            The id of the title to search.
+
+        Returns
+        -------
+        dict
+            A dictionary with the following properties:
+            title, genres, languages, type (movie or series), year, image_url, runtime,
+            plot, directors, stars, content_rating, imdb_rating, imdb_votes, metacritic_rating.
+        '''
+
         url = '{0}/Title/{1}/{2}'.format(
             self.base_url, self.api_key, title_id)
 
-        response = r.get(url)
+        response = requests.get(url)
 
-        if (response.status_code == 200):
+        if response.status_code == 200:
             response_obj = response.json()
-            return Title(
-                response_obj['fullTitle'],
-                response_obj['genres'],
-                response_obj['languages'],
-                response_obj['type'],
-                response_obj['year'],
-                response_obj['image'],
-                response_obj['runtimeStr'],
-                response_obj['plot'],
-                response_obj['directors'],
-                response_obj['stars'],
-                response_obj['contentRating'],
-                response_obj['imDbRating'],
-                response_obj['imDbRatingVotes'],
-                response_obj['metacriticRating']
-            )
+            return {
+                'title': response_obj['fullTitle'],
+                'genres': response_obj['genres'],
+                'languages': response_obj['languages'],
+                'type': response_obj['type'],
+                'year': response_obj['year'],
+                'image_url': response_obj['image'],
+                'runtime': response_obj['runtimeStr'],
+                'plot': response_obj['plot'],
+                'directors': response_obj['directors'],
+                'stars': response_obj['stars'],
+                'content_rating': response_obj['contentRating'],
+                'imdb_rating': response_obj['imDbRating'],
+                'imdb_votes': response_obj['imDbRatingVotes'],
+                'metacritic_rating': response_obj['metacriticRating']
+            }
 
-
-class Movie:
-    def __init__(self, id, title, description, image_url) -> None:
-        self.id = id
-        self.title = title
-        self.description = description
-        self.image_url = image_url
-
-
-class Title:
-    def __init__(self, full_title, genre, language, type, year, image_url, runtime, plot, directors, stars, content_rating, imdb_rating, imdb_votes, metacritic_rating) -> None:
-        self.full_title = full_title
-        self.genre = genre
-        self.language = language
-        self.type = type
-        self.year = year
-        self.image_url = image_url
-        self.runtime = runtime
-        self.plot = plot
-        self.directors = directors
-        self.stars = stars
-        self.content_rating = content_rating
-        self.imdb_rating = imdb_rating
-        self.imdb_votes = imdb_votes
-        self.metacritic_rating = metacritic_rating
+        return {}
