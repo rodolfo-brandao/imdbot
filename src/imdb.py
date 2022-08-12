14b1062f2534a6d3b014c12149f15210ef4abddf
Rodolfo <rodolfobrandao1995@gmail.com>
2022-08-11 22:27:45 -0300

    Update imdb.py

--- src/imdb.py
+++ src/imdb.py
@@ -9,7 +9,7 @@ class Imdb:
         self.base_url = 'https://imdb-api.com/en/API'
 
     def search(self, name: str, search_type: str = 'movie') -> list[dict]:
-        """Searches for occurences of movies or series whose title matches the specified name.
+        """Searches for occurences of movies or series whose title/description matches the specified name.
 
         Parameters
         ----------
@@ -42,8 +42,8 @@ class Imdb:
 
         return occurences
 
-    def search_title(self, title_id: str) -> dict:
-        """Searchs for the title specified by the id.
+    def details(self, id: str) -> dict:
+        """Searches for details of the title specified by its id.
 
         Parameters
         ----------
@@ -57,30 +57,30 @@ class Imdb:
             title, genres, languages, type (movie or series), year, image_url, runtime,
             plot, directors, stars, content_rating, imdb_rating, imdb_votes, metacritic_rating."""
 
-        url = f'{self.base_url}/Title/{self.api_key}/{title_id}'
+        url = f'{self.base_url}/Title/{self.api_key}/{id}'
         response = requests.get(url)
         details: dict = {}
 
         if response.status_code == 200:
-            response_obj = response.json()
+            response_obj: dict = response.json()
             details = {
-                'title': response_obj['fullTitle'],
-                'genres': response_obj['genres'],
-                'languages': response_obj['languages'],
-                'type': response_obj['type'],
-                'year': response_obj['year'],
-                'image_url': response_obj['image'],
-                'runtime': response_obj['runtimeStr'],
-                'plot': response_obj['plot'],
-                'directors': response_obj['directors'],
-                'stars': response_obj['stars'],
-                'content_rating': response_obj['contentRating'],
-                'imdb_rating': response_obj['imDbRating'],
-                'imdb_votes': response_obj['imDbRatingVotes'],
-                'metacritic_rating': response_obj['metacriticRating']
+                'title': self.__safe_get(response_obj, 'fullTitle'),
+                'genres': self.__safe_get(response_obj, 'genres'),
+                'languages': self.__safe_get(response_obj, 'languages'),
+                'type': self.__safe_get(response_obj, 'type'),
+                'year': self.__safe_get(response_obj, 'year'),
+                'image_url': self.__safe_get(response_obj, 'image'),
+                'runtime': self.__safe_get(response_obj, 'runtimeStr'),
+                'plot': self.__safe_get(response_obj, 'plot'),
+                'directors': self.__safe_get(response_obj, 'directors'),
+                'stars': self.__safe_get(response_obj, 'stars'),
+                'content_rating': self.__safe_get(response_obj, 'contentRating'),
+                'imdb_rating': self.__safe_get(response_obj, 'imDbRating'),
+                'imdb_votes': self.__safe_get(response_obj, 'imDbRatingVotes'),
+                'metacritic_rating':  self.__safe_get(response_obj, 'metacriticRating')
             }
 
-            for key in details:
-                details[key] = '?' if details[key] == '' or details[key] is None else details[key]
-
         return details
+
+    def __safe_get(self, arg: dict, key: str) -> str:
+        return arg.get(key) or '?'
